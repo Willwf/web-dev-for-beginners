7a7a0d828ee4f2bec984cbeddf759a77544cc12f
William Firmino <williamwf07@gmail.com>
2021-09-20 23:50:26 -0300

    Toggle the input event listener when start is clicked
    Disabled the input event listener on completion, and re-enable it when the button is clicked

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,15 +1,15 @@
 // all of our quotes
 const quotes = [
-  'I am a brain, Watson. The rest of me is a mere appendix.',
-  'There is nothing more deceptive than an obvious fact.',
-  'The lowest and vilest alleys in London do not present a more dreadful record of sin than does the smiling and beautiful countryside.',
-  'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
-  'There is nothing more deceptive than an obvious fact.',
-  'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
-  'I never make exceptions. An exception disproves the rule.',
-  'What one man can invent another can discover.',
-  'Nothing clears up a case so much as stating it to another person.',
-  'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+  "I am a brain, Watson. The rest of me is a mere appendix.",
+  "There is nothing more deceptive than an obvious fact.",
+  "The lowest and vilest alleys in London do not present a more dreadful record of sin than does the smiling and beautiful countryside.",
+  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
+  "There is nothing more deceptive than an obvious fact.",
+  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
+  "I never make exceptions. An exception disproves the rule.",
+  "What one man can invent another can discover.",
+  "Nothing clears up a case so much as stating it to another person.",
+  "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
 ];
 // store the list of words and the index of the word the player is currently typing
 let words = [];
@@ -17,41 +17,45 @@ let wordIndex = 0;
 // the starting time
 let startTime = Date.now();
 // page elements
-const quoteElement = document.getElementById('quote');
-const messageElement = document.getElementById('message');
-const typedValueElement = document.getElementById('typed-value');
+const quoteElement = document.getElementById("quote");
+const messageElement = document.getElementById("message");
+const typedValueElement = document.getElementById("typed-value");
+const startButton = document.getElementById("start");
 
-document.getElementById('start').addEventListener('click', () => {
+function handleQuote() {
   // get a quote
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[quoteIndex];
   // Put the quote into an array of words
-  words = quote.split(' ');
+  words = quote.split(" ");
   // reset the word index for tracking
   wordIndex = 0;
 
   // UI updates
   // Create an array of span elements so we can set a class
-  const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+  const spanWords = words.map(function (word) {
+    return `<span>${word} </span>`;
+  });
   // Convert into string and set as innerHTML on quote display
-  quoteElement.innerHTML = spanWords.join('');
+  quoteElement.innerHTML = spanWords.join("");
   // Highlight the first word
-  quoteElement.childNodes[0].className = 'highlight';
+  quoteElement.childNodes[0].className = "highlight";
   // Clear any prior messages
-  messageElement.innerText = '';
+  messageElement.innerText = "";
 
   // Setup the textbox
   // Clear the textbox
-  typedValueElement.value = '';
+  typedValueElement.value = "";
   // set focus
   typedValueElement.focus();
   // set the event handler
 
   // Start the timer
   startTime = new Date().getTime();
-});
+  typedValueElement.addEventListener("input", verifyTypedWords);
+}
 
-typedValueElement.addEventListener('input', () => {
+function verifyTypedWords() {
   // Get the current word
   const currentWord = words[wordIndex];
   // get the current value
@@ -61,26 +65,31 @@ typedValueElement.addEventListener('input', () => {
     // end of sentence
     // Display success
     const elapsedTime = new Date().getTime() - startTime;
-    const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+    const message = `CONGRATULATIONS! You finished in ${
+      elapsedTime / 1000
+    } seconds.`;
     messageElement.innerText = message;
-  } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+    typedValueElement.removeEventListener("input", verifyTypedWords);
+  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
     // end of word
     // clear the typedValueElement for the new word
-    typedValueElement.value = '';
+    typedValueElement.value = "";
     // move to the next word
     wordIndex++;
     // reset the class name for all elements in quote
     for (const wordElement of quoteElement.childNodes) {
-      wordElement.className = '';
+      wordElement.className = "";
     }
     // highlight the new word
-    quoteElement.childNodes[wordIndex].className = 'highlight';
+    quoteElement.childNodes[wordIndex].className = "highlight";
   } else if (currentWord.startsWith(typedValue)) {
     // currently correct
     // highlight the next word
-    typedValueElement.className = '';
+    typedValueElement.className = "";
   } else {
     // error state
-    typedValueElement.className = 'error';
+    typedValueElement.className = "error";
   }
-});
+}
+
+startButton.addEventListener("click", handleQuote);
