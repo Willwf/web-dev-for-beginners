90a2d6d788f7dd47b04dc0e70c8ef3c0647274a2
William Firmino <williamwf07@gmail.com>
2021-09-21 03:13:16 -0300

    Started storing high scores using localStorage

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,15 +1,49 @@
 // all of our quotes
 const quotes = [
-  "I am a brain, Watson. The rest of me is a mere appendix.",
-  "There is nothing more deceptive than an obvious fact.",
-  "The lowest and vilest alleys in London do not present a more dreadful record of sin than does the smiling and beautiful countryside.",
-  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
-  "There is nothing more deceptive than an obvious fact.",
-  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
-  "I never make exceptions. An exception disproves the rule.",
-  "What one man can invent another can discover.",
-  "Nothing clears up a case so much as stating it to another person.",
-  "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
+  {
+    id: "1",
+    quote: "I am a brain, Watson. The rest of me is a mere appendix.",
+  },
+  {
+    id: "2",
+    quote: "There is nothing more deceptive than an obvious fact.",
+  },
+  {
+    id: "3",
+    quote:
+      "The lowest and vilest alleys in London do not present a more dreadful record of sin than does the smiling and beautiful countryside.",
+  },
+  {
+    id: "4",
+    quote:
+      "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
+  },
+  {
+    id: "5",
+    quote: "There is nothing more deceptive than an obvious fact.",
+  },
+  {
+    id: "6",
+    quote:
+      "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
+  },
+  {
+    id: "7",
+    quote: "I never make exceptions. An exception disproves the rule.",
+  },
+  {
+    id: "8",
+    quote: "What one man can invent another can discover.",
+  },
+  {
+    id: "9",
+    quote: "Nothing clears up a case so much as stating it to another person.",
+  },
+  {
+    id: "10",
+    quote:
+      "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
+  },
 ];
 // store the list of words and the index of the word the player is currently typing
 let words = [];
@@ -23,11 +57,24 @@ const typedValueElement = document.getElementById("typed-value");
 const startButton = document.getElementById("start");
 const modal = document.getElementById("modal");
 const close = document.querySelector("#modal div a");
+const quoteScore = document.getElementById("quoteScore");
 
 function handleQuote() {
-  // get a quote
+  // get a quote and quote id
   const quoteIndex = Math.floor(Math.random() * quotes.length);
-  const quote = quotes[quoteIndex];
+  const quote = quotes[quoteIndex].quote;
+  const quoteId = quotes[quoteIndex].id;
+
+  quoteElement.dataset.id = quoteId;
+
+  if (localStorage.getItem(quoteId)) {
+    quoteScore.innerText = `Highest Score: ${localStorage.getItem(
+      quoteId
+    )} seconds. 🥇`;
+  } else {
+    quoteScore.innerText = "";
+  }
+
   // Put the quote into an array of words
   words = quote.split(" ");
   // reset the word index for tracking
@@ -63,23 +110,44 @@ function verifyTypedWords() {
   const currentWord = words[wordIndex];
   // get the current value
   const typedValue = typedValueElement.value;
+  // get the current id
+  const quoteId = quoteElement.dataset.id;
 
   if (typedValue === currentWord && wordIndex === words.length - 1) {
     // end of sentence
     // Display success
-    const elapsedTime = new Date().getTime() - startTime;
-    const message = `CONGRATULATIONS! You finished in ${
-      elapsedTime / 1000
-    } seconds.`;
-    messageElement.innerText = message;
+    const elapsedTime = (new Date().getTime() - startTime) / 1000;
+    const message = `CONGRATULATIONS! You finished in ${elapsedTime} seconds.`;
+    if (elapsedTime < localStorage.getItem(quoteId)) {
+      messageElement.innerText = message + ` New High Score! 🎉`;
+    } else {
+      messageElement.innerText = message;
+    }
+
     modal.classList.toggle("hide");
     close.addEventListener("click", () => {
       modal.classList.add("hide");
-      quoteElement.innerHTML = "";
+      quoteScore.innerText = `Highest Score: ${localStorage.getItem(
+        quoteId
+      )} seconds. 🥇`;
     });
+
     typedValueElement.removeEventListener("input", verifyTypedWords);
+
     typedValueElement.value = "";
     typedValueElement.setAttribute("readonly", "readonly");
+
+    function setScore() {
+      if (localStorage.getItem(quoteId)) {
+        if (elapsedTime < localStorage.getItem(quoteId)) {
+          localStorage.setItem(quoteId, elapsedTime);
+        }
+      } else {
+        localStorage.setItem(quoteId, elapsedTime);
+      }
+    }
+
+    setScore();
   } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
     // end of word
     // clear the typedValueElement for the new word
